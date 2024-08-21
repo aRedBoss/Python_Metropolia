ea5c74a0f9cc05ca5a613eef3be29db84daab13b
aRedBoss <aldulaimiomar111@gmail.com>
2024-08-21 20:00:13 +0300

    py

---
+++ Moduuli 2/6.py
@@ -0,0 +1,15 @@
+import random
+
+threeNum1 = random.randint(0, 9)
+threeNum2 = random.randint(0, 9)
+threeNum3 = random.randint(0, 9)
+
+fourNum1 = random.randint(1, 6)
+fourNum2 = random.randint(1, 6)
+fourNum3 = random.randint(1, 6)
+fourNum4 = random.randint(1, 6)
+
+print(f"{threeNum1}{threeNum2}{threeNum3}")
+print(f"{fourNum1}{fourNum2}{fourNum3}{fourNum4}")
+
+
