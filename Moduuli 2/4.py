7bdcefd66d9d622cd81e5eddd317e8f163675072
aRedBoss <aldulaimiomar111@gmail.com>
2024-08-21 15:57:05 +0300

    py

---
+++ Moduuli 2/4.py
@@ -0,0 +1,9 @@
+first_num = int(input("Give me first number: "))
+second_num = int(input("Give me second number: "))
+third_num = int(input("Give me third number: "))
+
+summma = first_num + second_num + third_num
+tulo = first_num * second_num * third_num
+keski_arvo = (first_num + second_num + third_num) / 2
+
+print(f"Summa: {summma}\n Tulo: {tulo}\n Keski arvo: {keski_arvo}")
