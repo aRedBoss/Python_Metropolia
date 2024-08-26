514652535a1f1bb72183556172f545d97a2c8154
aRedBoss <aldulaimiomar111@gmail.com>
2024-08-26 10:20:26 +0300

    py

--- Moduuli 2/6.py
+++ Moduuli 2/6.py
@@ -1,15 +1,6 @@
-import random
+from random import randint
 
-threeNum1 = random.randint(0, 9)
-threeNum2 = random.randint(0, 9)
-threeNum3 = random.randint(0, 9)
-
-fourNum1 = random.randint(1, 6)
-fourNum2 = random.randint(1, 6)
-fourNum3 = random.randint(1, 6)
-fourNum4 = random.randint(1, 6)
-
-print(f"{threeNum1}{threeNum2}{threeNum3}")
-print(f"{fourNum1}{fourNum2}{fourNum3}{fourNum4}")
+print(f"{randint(0, 9)}{randint(0, 9)}{randint(0, 9)}")
+print(f"{randint(1, 6)}{randint(1, 6)}{randint(1, 6)}{randint(1, 6)}")
 
 
